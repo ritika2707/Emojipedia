1bf854d7c83ac1f3f6960e838003fd1057c8a9a3
ritika2707 <108430007+ritika2707@users.noreply.github.com>
2023-05-18 02:51:16 +0530

    Add files via upload

---
+++ emojipedia/src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "😀",
+    name: "Grinning Face",
+    meaning:
+      "The grinning face expresses joy, happiness and satisfaction. It is often used to simply say hello, to express joy or excitement about something, or to break up a short text. Happiness and good mood are other associations associated with this emoji."
+  },
+  {
+    id: 5,
+    emoji: "🤨",
+    name: "Face With a Raised Eyebrow",
+    meaning:
+      "A raised-eyebrow emoji gives a questioning and skeptical look. It can be used to express skepticism, disbelief, or disapproval. This emoji is great for situations where you want to question a statement or action, take a critical stance, or make someone think in a humorous way."
+  },
+  {
+    id: 6,
+    emoji: "😎",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Smart and casual, this emoji features a face with a wide, closed smile and black sunglasses. It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent. It is also a symbol of sunshine, summer vacations, and beach vacations."
+  }
+];
+
+export default emojipedia;
